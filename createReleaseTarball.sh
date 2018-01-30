0eb8cd8af06e84e63ecb2e13815760a5a8a75a54
Hiranya Jayathilaka <hiranya911@gmail.com>
2018-01-29 16:48:33 -0800

    Removing # Unreleased with sed instead of replacing

--- createReleaseTarball.sh
+++ createReleaseTarball.sh
@@ -92,7 +92,7 @@ echo
 #  UPDATE CHANGELOG.md  #
 #########################
 echo "[INFO] Updating version number in CHANGELOG.md to ${VERSION_WITHOUT_RC}..."
-sed -i '' -e "1 s/# Unreleased//" CHANGELOG.md
+sed -i '' -e "/^# Unreleased$/d" CHANGELOG.md
 echo -e "# Unrelased\n\n-\n\n# v${VERSION_WITHOUT_RC}" | cat - CHANGELOG.md > TEMP_CHANGELOG.md
 mv TEMP_CHANGELOG.md CHANGELOG.md
 
