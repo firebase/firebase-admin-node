1190d188518439ce28da77fa8d2449b3dae2ce7a
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-02-13 11:03:38 -0800

    Fleshed out the full workflow

--- .github/scripts/publish_preflight_check.sh
+++ .github/scripts/publish_preflight_check.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ###################################### Outputs #####################################
 
 # 1. version: The version of this release including the 'v' prefix (e.g. v1.2.3).
@@ -75,6 +89,13 @@ if [[ -f "${RELEASE_ARTIFACT}" ]]; then
   echo_info "Found release artifact: ${RELEASE_ARTIFACT}"
 else
   echo_warn "Release artifact ${RELEASE_ARTIFACT} not found."
+  terminate
+fi
+
+readonly ARTIFACT_COUNT=`ls dist/ | wc -l`
+if [[ $ARTIFACT_COUNT -ne 1 ]]; then
+  echo_warn "Unexpected artifacts in the distribution directory."
+  ls -l dist
   terminate
 fi
 
