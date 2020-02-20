7c8fb9378af19ec4a9af52ea06f70b997c8bd2e5
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-02-20 15:14:24 -0800

    Added licesne header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
+/*!
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
@@ -11,7 +27,7 @@ module.exports = {
   ],
   rules: {
     // Following checks are temporarily disabled. We shall incrementally enable them in the
-    // future, fixing any violation as we go.
+    // future, fixing any violations as we go.
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-use-before-define": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
