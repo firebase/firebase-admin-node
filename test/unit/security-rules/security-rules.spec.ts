7185dc546e899da29baa971e632ce31142d6ec7e
MathBunny <horatiulazu@gmail.com>
2020-08-05 16:52:20 -0400

    Add newline at eof, shuffle import

--- test/unit/security-rules/security-rules.spec.ts
+++ test/unit/security-rules/security-rules.spec.ts
@@ -19,11 +19,11 @@
 import * as _ from 'lodash';
 import * as chai from 'chai';
 import * as sinon from 'sinon';
+import { SecurityRules } from '../../../src/security-rules/security-rules';
 import { FirebaseApp } from '../../../src/firebase-app';
 import * as mocks from '../../resources/mocks';
 import { SecurityRulesApiClient, RulesetContent } from '../../../src/security-rules/security-rules-api-client-internal';
 import { FirebaseSecurityRulesError } from '../../../src/security-rules/security-rules-utils-internal';
-import { SecurityRules } from '../../../src/security-rules/security-rules';
 import { deepCopy } from '../../../src/utils/deep-copy';
 
 const expect = chai.expect;
