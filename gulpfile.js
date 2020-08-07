5f01480aa059cab62dcad3bc710b7b377e2b27e6
MathBunny <horatiulazu@gmail.com>
2020-08-07 14:24:25 -0400

    fix: Reflect changes in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,8 @@ var paths = {
     'src/*.d.ts',
     '!src/instance-id.d.ts',
     '!src/security-rules.d.ts',
-    '!src/project-management.d.ts'
+    '!src/project-management.d.ts',
+    '!src/remote-config.d.ts'
   ],
 };
 
@@ -70,7 +71,6 @@ const TEMPORARY_TYPING_EXCLUDES = [
   '!lib/firestore/*.d.ts',
   '!lib/machine-learning/*.d.ts',
   '!lib/messaging/*.d.ts',
-  '!lib/remote-config/*.d.ts',
   '!lib/storage/*.d.ts',
   '!lib/utils/*.d.ts'
 ];
