3845514d986a4a66f82318be49e8d752f19aafed
MathBunny <horatiulazu@gmail.com>
2020-08-22 22:07:49 -0400

    fix: General cleanup

--- src/credential.d.ts
+++ src/credential.d.ts
@@ -30,36 +30,36 @@ export namespace admin.credential {
   /**
    * Returns a credential created from the
    * {@link
-    *    https://developers.google.com/identity/protocols/application-default-credentials
-    *    Google Application Default Credentials}
-    * that grants admin access to Firebase services. This credential can be used
-    * in the call to
-    * {@link
-    *   https://firebase.google.com/docs/reference/admin/node/admin#.initializeApp
-    *  `admin.initializeApp()`}.
-    *
-    * Google Application Default Credentials are available on any Google
-    * infrastructure, such as Google App Engine and Google Compute Engine.
-    *
-    * See
-    * {@link
-    *   https://firebase.google.com/docs/admin/setup#initialize_the_sdk
-    *   Initialize the SDK}
-    * for more details.
-    *
-    * @example
-    * ```javascript
-    * admin.initializeApp({
-    *   credential: admin.credential.applicationDefault(),
-    *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-    * });
-    * ```
-    *
-    * @param {!Object=} httpAgent Optional [HTTP Agent](https://nodejs.org/api/http.html#http_class_http_agent)
-    *   to be used when retrieving access tokens from Google token servers.
-    *
-    * @return {!admin.credential.Credential} A credential authenticated via Google
-    *   Application Default Credentials that can be used to initialize an app.
+   *    https://developers.google.com/identity/protocols/application-default-credentials
+   *    Google Application Default Credentials}
+   * that grants admin access to Firebase services. This credential can be used
+   * in the call to
+   * {@link
+   *   https://firebase.google.com/docs/reference/admin/node/admin#.initializeApp
+   *  `admin.initializeApp()`}.
+   *
+   * Google Application Default Credentials are available on any Google
+   * infrastructure, such as Google App Engine and Google Compute Engine.
+   *
+   * See
+   * {@link
+   *   https://firebase.google.com/docs/admin/setup#initialize_the_sdk
+   *   Initialize the SDK}
+   * for more details.
+   *
+   * @example
+   * ```javascript
+   * admin.initializeApp({
+   *   credential: admin.credential.applicationDefault(),
+   *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+   * });
+   * ```
+   *
+   * @param {!Object=} httpAgent Optional [HTTP Agent](https://nodejs.org/api/http.html#http_class_http_agent)
+   *   to be used when retrieving access tokens from Google token servers.
+   *
+   * @return {!admin.credential.Credential} A credential authenticated via Google
+   *   Application Default Credentials that can be used to initialize an app.
    */
   function applicationDefault(httpAgent?: Agent): admin.credential.Credential;
 
@@ -68,45 +68,45 @@ export namespace admin.credential {
    * admin access to Firebase services. This credential can be used in the call
    * to
    * {@link
-    *   https://firebase.google.com/docs/reference/admin/node/admin#.initializeApp
-    *   `admin.initializeApp()`}.
-    *
-    * See
-    * {@link
-    *   https://firebase.google.com/docs/admin/setup#initialize_the_sdk
-    *   Initialize the SDK}
-    * for more details.
-    *
-    * @example
-    * ```javascript
-    * // Providing a path to a service account key JSON file
-    * var serviceAccount = require("path/to/serviceAccountKey.json");
-    * admin.initializeApp({
-    *   credential: admin.credential.cert(serviceAccount),
-    *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-    * });
-    * ```
-    *
-    * @example
-    * ```javascript
-    * // Providing a service account object inline
-    * admin.initializeApp({
-    *   credential: admin.credential.cert({
-    *     projectId: "<PROJECT_ID>",
-    *     clientEmail: "foo@<PROJECT_ID>.iam.gserviceaccount.com",
-    *     privateKey: "-----BEGIN PRIVATE KEY-----<KEY>-----END PRIVATE KEY-----\n"
-    *   }),
-    *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-    * });
-    * ```
-    *
-    * @param serviceAccountPathOrObject The path to a service
-    *   account key JSON file or an object representing a service account key.
-    * @param httpAgent Optional [HTTP Agent](https://nodejs.org/api/http.html#http_class_http_agent)
-    *   to be used when retrieving access tokens from Google token servers.
-    *
-    * @return A credential authenticated via the
-    *   provided service account that can be used to initialize an app.
+   *   https://firebase.google.com/docs/reference/admin/node/admin#.initializeApp
+   *   `admin.initializeApp()`}.
+   *
+   * See
+   * {@link
+   *   https://firebase.google.com/docs/admin/setup#initialize_the_sdk
+   *   Initialize the SDK}
+   * for more details.
+   *
+   * @example
+   * ```javascript
+   * // Providing a path to a service account key JSON file
+   * var serviceAccount = require("path/to/serviceAccountKey.json");
+   * admin.initializeApp({
+   *   credential: admin.credential.cert(serviceAccount),
+   *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+   * });
+   * ```
+   *
+   * @example
+   * ```javascript
+   * // Providing a service account object inline
+   * admin.initializeApp({
+   *   credential: admin.credential.cert({
+   *     projectId: "<PROJECT_ID>",
+   *     clientEmail: "foo@<PROJECT_ID>.iam.gserviceaccount.com",
+   *     privateKey: "-----BEGIN PRIVATE KEY-----<KEY>-----END PRIVATE KEY-----\n"
+   *   }),
+   *   databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+   * });
+   * ```
+   *
+   * @param serviceAccountPathOrObject The path to a service
+   *   account key JSON file or an object representing a service account key.
+   * @param httpAgent Optional [HTTP Agent](https://nodejs.org/api/http.html#http_class_http_agent)
+   *   to be used when retrieving access tokens from Google token servers.
+   *
+   * @return A credential authenticated via the
+   *   provided service account that can be used to initialize an app.
    */
   function cert(serviceAccountPathOrObject: string | _admin.ServiceAccount, httpAgent?: Agent): admin.credential.Credential;
 
