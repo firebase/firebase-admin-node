7df63d10fbade985fc851f4479640a444eb3feb1
Samuel Bushi <ssbushi@google.com>
2021-03-11 16:31:03 +0000

    Add test for FIREBASE_STORAGE_EMULATOR_HOST support

--- test/unit/storage/storage.spec.ts
+++ test/unit/storage/storage.spec.ts
@@ -29,88 +29,110 @@ describe('Storage', () => {
   let mockCredentialApp: FirebaseApp;
   let storage: Storage;
 
-  beforeEach(() => {
-    mockApp = mocks.app();
-    mockCredentialApp = mocks.mockCredentialApp();
-    storage = new Storage(mockApp);
-  });
-
-  afterEach(() => {
-    return mockApp.delete();
-  });
-
-  describe('Constructor', () => {
-    const invalidApps = [null, NaN, 0, 1, true, false, '', 'a', [], [1, 'a'], {}, { a: 1 }, _.noop];
-    invalidApps.forEach((invalidApp) => {
-      it(`should throw given invalid app: ${ JSON.stringify(invalidApp) }`, () => {
+  describe('Storage App', () => {
+    beforeEach(() => {
+      mockApp = mocks.app();
+      mockCredentialApp = mocks.mockCredentialApp();
+      storage = new Storage(mockApp);
+    });
+  
+    afterEach(() => {
+      return mockApp.delete();
+    });
+  
+    describe('Constructor', () => {
+      const invalidApps = [null, NaN, 0, 1, true, false, '', 'a', [], [1, 'a'], {}, { a: 1 }, _.noop];
+      invalidApps.forEach((invalidApp) => {
+        it(`should throw given invalid app: ${ JSON.stringify(invalidApp) }`, () => {
+          expect(() => {
+            const storageAny: any = Storage;
+            return new storageAny(invalidApp);
+          }).to.throw('First argument passed to admin.storage() must be a valid Firebase app instance.');
+        });
+      });
+  
+      it('should throw given no app', () => {
         expect(() => {
           const storageAny: any = Storage;
-          return new storageAny(invalidApp);
+          return new storageAny();
         }).to.throw('First argument passed to admin.storage() must be a valid Firebase app instance.');
       });
-    });
-
-    it('should throw given no app', () => {
-      expect(() => {
-        const storageAny: any = Storage;
-        return new storageAny();
-      }).to.throw('First argument passed to admin.storage() must be a valid Firebase app instance.');
-    });
-
-    it('should throw given invalid credential', () => {
-      const expectedError = 'Failed to initialize Google Cloud Storage client with the available ' +
-        'credential. Must initialize the SDK with a certificate credential or application default ' +
-        'credentials to use Cloud Storage API.';
-      expect(() => {
-        const storageAny: any = Storage;
-        return new storageAny(mockCredentialApp);
-      }).to.throw(expectedError);
-    });
-
-    it('should not throw given a valid app', () => {
-      expect(() => {
-        return new Storage(mockApp);
-      }).not.to.throw();
-    });
-  });
-
-  describe('app', () => {
-    it('returns the app from the constructor', () => {
-      // We expect referential equality here
-      expect(storage.app).to.equal(mockApp);
-    });
-
-    it('is read-only', () => {
-      expect(() => {
-        (storage as any).app = mockApp;
-      }).to.throw('Cannot set property app of #<Storage> which has only a getter');
-    });
-  });
-
-  describe('bucket(invalid)', () => {
-    const expectedError = 'Bucket name not specified or invalid. Specify a valid bucket name via ' +
-        'the storageBucket option when initializing the app, or specify the bucket name ' +
-        'explicitly when calling the getBucket() method.';
-    const invalidNames = [null, NaN, 0, 1, true, false, '', [], [1, 'a'], {}, { a: 1 }, _.noop];
-    invalidNames.forEach((invalidName) => {
-      it(`should throw given invalid bucket name: ${ JSON.stringify(invalidName) }`, () => {
+  
+      it('should throw given invalid credential', () => {
+        const expectedError = 'Failed to initialize Google Cloud Storage client with the available ' +
+          'credential. Must initialize the SDK with a certificate credential or application default ' +
+          'credentials to use Cloud Storage API.';
         expect(() => {
-          const bucketAny: any = storage.bucket;
-          bucketAny(invalidName);
+          const storageAny: any = Storage;
+          return new storageAny(mockCredentialApp);
         }).to.throw(expectedError);
+      });
+  
+      it('should not throw given a valid app', () => {
+        expect(() => {
+          return new Storage(mockApp);
+        }).not.to.throw();
+      });
+    });
+  
+    describe('app', () => {
+      it('returns the app from the constructor', () => {
+        // We expect referential equality here
+        expect(storage.app).to.equal(mockApp);
+      });
+  
+      it('is read-only', () => {
+        expect(() => {
+          (storage as any).app = mockApp;
+        }).to.throw('Cannot set property app of #<Storage> which has only a getter');
+      });
+    });
+  
+    describe('bucket(invalid)', () => {
+      const expectedError = 'Bucket name not specified or invalid. Specify a valid bucket name via ' +
+          'the storageBucket option when initializing the app, or specify the bucket name ' +
+          'explicitly when calling the getBucket() method.';
+      const invalidNames = [null, NaN, 0, 1, true, false, '', [], [1, 'a'], {}, { a: 1 }, _.noop];
+      invalidNames.forEach((invalidName) => {
+        it(`should throw given invalid bucket name: ${ JSON.stringify(invalidName) }`, () => {
+          expect(() => {
+            const bucketAny: any = storage.bucket;
+            bucketAny(invalidName);
+          }).to.throw(expectedError);
+        });
+      });
+    });
+  
+    describe('bucket()', () => {
+      it('should return a bucket object', () => {
+        expect(storage.bucket().name).to.equal('bucketName.appspot.com');
+      });
+    });
+  
+    describe('bucket(valid)', () => {
+      it('should return a bucket object', () => {
+        expect(storage.bucket('foo').name).to.equal('foo');
       });
     });
   });
 
-  describe('bucket()', () => {
-    it('should return a bucket object', () => {
-      expect(storage.bucket().name).to.equal('bucketName.appspot.com');
-    });
-  });
+  describe('Emulator Support', () => {
+    it('sets STORAGE_EMULATOR_HOST if FIREBASE_STORAGE_EMULATOR_HOST is set', () => {
+      const EMULATOR_HOST = "http://localhost:9199";
+      delete process.env.STORAGE_EMULATOR_HOST;
+      process.env.FIREBASE_STORAGE_EMULATOR_HOST = EMULATOR_HOST;
 
-  describe('bucket(valid)', () => {
-    it('should return a bucket object', () => {
-      expect(storage.bucket('foo').name).to.equal('foo');
+      mockApp = mocks.app();
+      mockCredentialApp = mocks.mockCredentialApp();
+      storage = new Storage(mockApp);
+
+      expect(process.env.STORAGE_EMULATOR_HOST).to.equal(EMULATOR_HOST);
     });
-  });
+  
+    afterEach(() => {
+      delete process.env.STORAGE_EMULATOR_HOST;
+      delete process.env.FIREBASE_STORAGE_EMULATOR_HOST;
+      return mockApp.delete();
+    });
+  })
 });
