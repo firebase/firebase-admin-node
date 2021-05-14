20f6494e853392ecfcb14459b18a5099aace93f3
Hiranya Jayathilaka <hiranya911@gmail.com>
2021-05-13 18:05:08 -0700

    chore: Cleaned up the post processing script

--- docgen/post-process.js
+++ docgen/post-process.js
@@ -1,30 +1,51 @@
+/**
+ * @license
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const fs = require('mz/fs');
 const path = require('path');
 const readline = require('readline');
 
-async function getExtras() {
+async function main() {
+  const extras = await getExtraFiles();
+  for (const source of extras) {
+    await applyExtraContentFrom(source);
+  }
+}
+
+async function getExtraFiles() {
   const extrasPath = path.join(__dirname, 'extras');
   const files = await fs.readdir(extrasPath);
-  return files.filter((name) => name.endsWith('.md'));
+  return files
+    .filter((name) => name.endsWith('.md'))
+    .map((name) => path.join(__dirname, 'extras', name));
 }
 
-async function getContentFrom(source) {
-  const reader = readline.createInterface({
-    input: fs.createReadStream(source),
-  });
-
-  const content = ['\n'];
-  for await (const line of reader) {
-    content.push(line);
+async function applyExtraContentFrom(source) {
+  const target = path.join(__dirname, 'markdown', path.basename(source));
+  if (!await fs.exists(target)) {
+    console.log(`Target path not found: ${target}`);
+    return;
   }
 
-  return content;
+  const extra = await readExtraContentFrom(source);
+  await writeExtraContentTo(target, extra);
 }
 
-async function applyExtra(source, target) {
-  console.log(`Applying extras to ${target}`);
-  const content = await getContentFrom(source);
-
+async function writeExtraContentTo(target, extra) {
   const output = [];
   const reader = readline.createInterface({
     input: fs.createReadStream(target),
@@ -32,30 +53,30 @@ async function applyExtra(source, target) {
   for await (const line of reader) {
     output.push(line);
     if (line.startsWith('{% block body %}')) {
-      output.push(...content);
+      output.push(...extra);
     }
   }
 
   const outputBuffer = Buffer.from(output.join('\r\n'));
+  console.log(`Writing extra content to ${target}`);
   await fs.writeFile(target, outputBuffer);
 }
 
-async function postProcessFiles() {
-  const extras = await getExtras();
-  for (const extra of extras) {
-    const source = path.join(__dirname, 'extras', extra);
-    const target = path.join(__dirname, 'markdown', extra);
-    if (await fs.exists(target)) {
-      await applyExtra(source, target);
-    } else {
-      console.log(`Target path not found: ${target}`);
-    }
+async function readExtraContentFrom(source) {
+  const reader = readline.createInterface({
+    input: fs.createReadStream(source),
+  });
+  const content = [''];
+  for await (const line of reader) {
+    content.push(line);
   }
+
+  return content;
 }
 
 (async () => {
   try {
-    await postProcessFiles();
+    await main();
   } catch (err) {
     console.log(err);
     process.exit(1);
