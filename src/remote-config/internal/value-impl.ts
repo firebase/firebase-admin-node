a28252c3468e5667be04c8b8fe2915fa927bf830
Erik Eldridge <erikeldridge@google.com>
2024-04-04 14:48:32 -0700

    Make Value constants readonly and define default boolean value.
    Per discussion on the PR, explicitly defining the default boolean
    value is more readable.

--- src/remote-config/internal/value-impl.ts
+++ src/remote-config/internal/value-impl.ts
@@ -29,24 +29,31 @@ import {
  * @internal
  */
 export class ValueImpl implements Value {
-  public static BOOLEAN_TRUTHY_VALUES = ['1', 'true', 't', 'yes', 'y', 'on'];
-  public static DEFAULT_VALUE_FOR_NUMBER = 0;
-  public static DEFAULT_VALUE_FOR_STRING = '';
+  public static readonly DEFAULT_VALUE_FOR_BOOLEAN = false;
+  public static readonly DEFAULT_VALUE_FOR_STRING = '';
+  public static readonly DEFAULT_VALUE_FOR_NUMBER = 0;
+  public static readonly BOOLEAN_TRUTHY_VALUES = ['1', 'true', 't', 'yes', 'y', 'on'];
   constructor(
-        private readonly source: ValueSource,
-        private readonly value = ValueImpl.DEFAULT_VALUE_FOR_STRING) { }
+    private readonly source: ValueSource,
+    private readonly value = ValueImpl.DEFAULT_VALUE_FOR_STRING) { }
+  asString(): string {
+    return this.value;
+  }
   asBoolean(): boolean {
+    if (this.source === 'static') {
+      return ValueImpl.DEFAULT_VALUE_FOR_BOOLEAN;
+    }
     return ValueImpl.BOOLEAN_TRUTHY_VALUES.indexOf(this.value.toLowerCase()) >= 0;
   }
   asNumber(): number {
+    if (this.source === 'static') {
+      return ValueImpl.DEFAULT_VALUE_FOR_NUMBER;
+    }
     const num = Number(this.value);
     if (isNaN(num)) {
       return ValueImpl.DEFAULT_VALUE_FOR_NUMBER;
     }
     return num;
-  }
-  asString(): string {
-    return this.value;
   }
   getSource(): ValueSource {
     return this.source;
