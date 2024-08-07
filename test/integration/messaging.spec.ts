ae885b353bb2f6dce31baa95f0ac048c0697b396
Jonathan Edey <jedey@google.com>
2024-08-07 10:30:54 -0400

    chore: Skip sendToDeviceGroup integration test

--- test/integration/messaging.spec.ts
+++ test/integration/messaging.spec.ts
@@ -278,7 +278,7 @@ describe('admin.messaging', () => {
       });
   });
 
-  it('sendToDeviceGroup() returns a response with success count', () => {
+  it.skip('sendToDeviceGroup() returns a response with success count', () => {
     return getMessaging().sendToDeviceGroup(notificationKey, payload, options)
       .then((response) => {
         expect(typeof response.successCount).to.equal('number');
