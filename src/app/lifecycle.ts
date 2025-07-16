daa9eb6b97d7594407b83e259a68b5050a08e2f2
DellaBitta <drsanta@google.com>
2025-07-16 10:26:51 -0400

    Changes based on review feedback.

--- src/app/lifecycle.ts
+++ src/app/lifecycle.ts
@@ -42,81 +42,44 @@ export class AppStore {
 
     // Check if an App already exists and, if so, ensure its AppOptions match
     // those of this `initializeApp` request. 
-    if (this.appStore.has(appName)) {
-      const currentApp = this.appStore.get(appName)!;
-      // Ensure the autoInit state matches the existing app's. If not, throw.
-      if (currentApp.autoInit() !== autoInit) {
-        throw new FirebaseAppError(
-          AppErrorCodes.INVALID_APP_OPTIONS,
-          `Firebase app named "${appName}" attempted mismatch between custom AppOptions` +
-          ' and an App created via Auto Init.'
-        )
-      } else if (autoInit) {
-        // Auto-initialization is triggered when no options were passed to
-        // initializeApp. With no options to compare, simply return the App.
-        return currentApp;
-      } else {
-        // Auto-initialization was not used. Ensure the options don't contain
-        // incomparable fields, and that options matches the existing app's.
-
-        // `httpAgent` objects cannot be compared with deepEquals impls.
-        // Idempotency cannot be supported if one exists.
-        if (typeof options.httpAgent !== 'undefined') {
-          throw new FirebaseAppError(
-            AppErrorCodes.INVALID_APP_OPTIONS,
-            `Firebase app named "${appName}" already exists and initializeApp was` +
-            ' invoked with an optional http.Agent. The SDK cannot confirm the equality' +
-            ' of http.Agent objects with the existing app. Please use getApp or getApps to reuse' +
-            ' the existing app instead.'
-          );
-        } else if (typeof currentApp.options.httpAgent !== 'undefined') {
-          throw new FirebaseAppError(
-            AppErrorCodes.INVALID_APP_OPTIONS,
-            `An existing app named "${appName}" already exists with a different` +
-            ' options configuration: httpAgent.'
-          );
-        }
-        
-        // `Credential` objects cannot be compared with deepEquals impls.
-        // Idempotency cannot be supported if one exists.
-        if (typeof options.credential !== 'undefined') {
-          throw new FirebaseAppError(
-            AppErrorCodes.INVALID_APP_OPTIONS,
-            `Firebase app named "${appName}" already exists and initializeApp was` +
-            ' invoked with an optional Credential. The SDK cannot confirm the equality' +
-            ' of Credential objects with the existing app. Please use getApp or getApps' +
-            ' to reuse the existing app instead.'
-          );
-        } else if (currentApp.customCredential()) {
-          throw new FirebaseAppError(
-            AppErrorCodes.INVALID_APP_OPTIONS,
-            `An existing app named "${appName}" already exists with a different` +
-            ' options configuration: Credential.'
-          );
-        }
-
-        // The AppOptions object has no known incomparable fields.
-
-        // FirebaseApp() aguments app.options with a synthesized Credential
-        // upon App construction (below). Run a comparison w/o Credential to
-        // see if the base configurations match. Return the existing app if so.
-        const currentAppOptions = { ...currentApp.options };
-        delete currentAppOptions.credential;
-        if (deepEqual(options, currentAppOptions)) {
-          return currentApp;
-        } else {
-          throw new FirebaseAppError(
-            AppErrorCodes.DUPLICATE_APP,
-            `A Firebase app named "${appName}" already exists with a different options` +
-            ' configuration.'
-          );
-        }
-      }
+    if (!this.appStore.has(appName)) {
+      const app = new FirebaseApp(options, appName, autoInit, this);
+      this.appStore.set(app.name, app);
+      return app;
     }
 
-    const app = new FirebaseApp(options, appName, autoInit, this);
-    this.appStore.set(app.name, app);
-    return app;
+    const currentApp = this.appStore.get(appName)!;
+    // Ensure the autoInit state matches the existing app's. If not, throw.
+    if (currentApp.autoInit() !== autoInit) {
+      throw new FirebaseAppError(
+        AppErrorCodes.INVALID_APP_OPTIONS,
+        `A Firebase app named "${appName}" already exists with a different configuration.`
+      )
+    }
+
+    if (autoInit) {
+      // Auto-initialization is triggered when no options were passed to
+      // initializeApp. With no options to compare, simply return the App.
+      return currentApp;
+    }
+
+    // Ensure the options objects don't break deep equal comparisons.
+    validateAppOptionsSupportDeepEquals(options, currentApp);
+
+    // FirebaseApp() adds a synthesized Credential to app.options upon App
+    // construction. Run a comparison w/o Credential to see if the base config
+    // matches. Return the existing app if so.
+    const currentAppOptions = { ...currentApp.options };
+    delete currentAppOptions.credential;
+    if (!deepEqual(options, currentAppOptions)) {
+      throw new FirebaseAppError(
+        AppErrorCodes.DUPLICATE_APP,
+        `A Firebase app named "${appName}" already exists with a different configuration.`
+      );
+
+    }
+
+    return currentApp;
   }
 
   public getApp(appName: string = DEFAULT_APP_NAME): App {
@@ -170,6 +133,59 @@ export class AppStore {
 }
 
 /**
+ * Validates that the `requestedOptions` and the `existingApp` options objects
+ * do not have fields that would break deep equals comparisons.
+ * 
+ * @param requestedOptions The incoming AppOptions of a new initailizeApp
+ *   request.
+ * @param existingApp An existing app with internal options to compare against.
+ * 
+ * @throws FirebaseAppError if the objects cannot be deeply compared.
+ * 
+ * @internal
+ */
+function validateAppOptionsSupportDeepEquals(
+  requestedOptions: AppOptions,
+  existingApp: FirebaseApp): void {
+
+  // http.Agent checks.
+  if (typeof requestedOptions.httpAgent !== 'undefined') {
+    throw new FirebaseAppError(
+      AppErrorCodes.INVALID_APP_OPTIONS,
+      `Firebase app named "${existingApp.name}" already exists and initializeApp was` +
+      ' invoked with an optional http.Agent. The SDK cannot confirm the equality' +
+      ' of http.Agent objects with the existing app. Please use getApp or getApps to reuse' +
+      ' the existing app instead.'
+    );
+  } else if (typeof existingApp.options.httpAgent !== 'undefined') {
+    throw new FirebaseAppError(
+      AppErrorCodes.INVALID_APP_OPTIONS,
+      `An existing app named "${existingApp.name}" already exists with a different` +
+      ' options configuration: httpAgent.'
+    );
+  }
+
+  // Credential checks.
+  if (typeof requestedOptions.credential !== 'undefined') {
+    throw new FirebaseAppError(
+      AppErrorCodes.INVALID_APP_OPTIONS,
+      `Firebase app named "${existingApp.name}" already exists and initializeApp was` +
+      ' invoked with an optional Credential. The SDK cannot confirm the equality' +
+      ' of Credential objects with the existing app. Please use getApp or getApps' +
+      ' to reuse the existing app instead.'
+    );
+  }
+
+  if (existingApp.customCredential()) {
+    throw new FirebaseAppError(
+      AppErrorCodes.INVALID_APP_OPTIONS,
+      `An existing app named "${existingApp.name}" already exists with a different` +
+      ' options configuration: Credential.'
+    );
+  }
+}
+
+/**
  * Checks to see if the provided appName is a non-empty string and throws if it
  * is not.
  * 
@@ -180,7 +196,7 @@ export class AppStore {
  * @internal
  */
 function validateAppNameFormat(appName: string): void {
-  if (typeof appName !== 'string' || appName === '') {
+  if (!validator.isNonEmptyString(appName)) {
     throw new FirebaseAppError(
       AppErrorCodes.INVALID_APP_NAME,
       `Invalid Firebase app name "${appName}" provided. App name must be a non-empty string.`,
@@ -200,6 +216,17 @@ export const defaultAppStore = new AppStore();
  * this function cannot support idempotency if either of `options.httpAgent` or
  * `options.credential` are defined. When either is defined, subsequent invocations will 
  * throw instead of returning an {@link App} object.
+ *
+ * For example, to safely initialize an app that may already exist:
+ * 
+ * ```javascript
+ * let app;
+ * try {
+ *   app = getApp("myApp");
+ * } catch (error) {
+ *   app = initializeApp({ credential: myCredential }, "myApp");
+ * }
+ * ```
  *
  * @param options - Optional A set of {@link AppOptions} for the {@link App} instance.
  *   If not present, `initializeApp` will try to initialize with the options from the
@@ -225,7 +252,7 @@ export function initializeApp(options?: AppOptions, appName: string = DEFAULT_AP
 
 /**
  * Returns an existing App instance for the provided name. If no name is provided
- * the the default app name is queried.
+ * the the default app name is used.
  *
  * @param appName - Optional name of the FirebaseApp instance.
  *
