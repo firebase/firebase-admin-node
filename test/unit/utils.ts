f622f823933eb829801ef48958c02723d6811cad
Lahiru Maramba <llahiru@gmail.com>
2026-06-05 15:12:25 -0400

    change: Remove Deprecated Legacy Namespace Support (#3164)
    * feat: remove legacy namespaces support
    
    * test(integration): migrate app-check integration tests to modular API
    
    * fix api-extractor
    
    ---------
    
    Co-authored-by: Jonathan Edey <145066863+jonathanedey@users.noreply.github.com>

--- test/unit/utils.ts
+++ test/unit/utils.ts
@@ -18,7 +18,7 @@
 import * as _ from 'lodash';
 import * as sinon from 'sinon';
 import * as mocks from '../resources/mocks';
-import { AppOptions } from '../../src/firebase-namespace-api';
+import { AppOptions } from '../../src/app/index';
 import { FirebaseApp, FirebaseAppInternals, FirebaseAccessToken } from '../../src/app/firebase-app';
 import { RequestResponseError, RequestResponse } from '../../src/utils/api-request';
 
